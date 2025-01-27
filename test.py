c9a6b75f25c8cfd1c74e4a0aafa5ecf2ecdf4af1
xhulio11 <xhuliano1998@gmail.com>
2025-01-27 11:32:06 +0200

    test

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
+import json 
 import sys 
 import os 
 
